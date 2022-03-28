4b4844dbc6c1c4b857c72811e6726138b00df117
ayhan <yilmax4@gmail.com>
2022-03-28 08:34:25 +0300

    file was created

---
+++ cel-fah.py
@@ -0,0 +1,4 @@
+print("Celsius temperature converts the  into Fahrenheit degree" )
+celsius = float(input("değer giriniz:"))
+print("hesaplanıyor!")
+print("fahrenheit degree:", float(celsius*1.8000+32.00))
