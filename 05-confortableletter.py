7c001eaf2019e38938b8308371957aecf3cfd5db
ayhan <yilmax4@gmail.com>
2022-03-29 19:44:14 +0300

    comfortable letter check code

---
+++ 05-confortableletter.py
@@ -0,0 +1,4 @@
+word =set(input("enter a word:"))                 # ASSİGNMENT 4 SON HALİ FARKLI YOLDAN
+left = set('qwertasdfgzxcvb')
+right = set('yuıopğühjklşinmöç')
+print(bool(word - left and word - right))
